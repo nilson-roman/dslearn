025d0b2785e4dba1c1f7776ee21479a4caa6cfac
nilson-roman <nilsonoroman@gmail.com>
2021-06-08 19:07:34 -0300

    Enrollment seed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -25,3 +25,6 @@ INSERT INTO tb_resource(title, description, position, img_Uri, type, offer_id) V
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequesite_id) VALUES ('Capítulo 1', 'Neste capítulo vamos começar', 1, 'https://www.stockvault.net/data/2018/09/15/254573/preview16.jpg', 1, null);
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequesite_id) VALUES ('Capítulo 2', 'Neste capítulo vamos continuar', 2, 'https://www.stockvault.net/data/2018/09/15/254573/preview16.jpg', 1, 1);
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequesite_id) VALUES ('Capítulo 3', 'Neste capítulo vamos finalizar', 3, 'https://www.stockvault.net/data/2018/09/15/254573/preview16.jpg', 1, 2);
+
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refound_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refound_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2021-11-20T13:00:00Z', null, true, false);
